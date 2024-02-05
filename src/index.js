8ffe04fa225d93465b647715dd245b103589e213
Yuri <+>
2024-02-05 08:58:40 -0500

    Completed TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buildTasks(event.target.new-tasks.value)
+    form.reset()
+  })
 });
+
+function buildTasks(tasks) {
+    let p = document.createElement('p')
+    let description = document.createElement('button')
+    description.addEventListener('click', toDelete)
+    description.textContent = 'x'
+    p.textContent = `${tasks}`
+    p.appendChild(description)
+    console.log(p)
+    document.querySelector('#tasks').appendChild(p)
+}
+
+function toDelete(event) {
+  event.target.parentNode.remove()
+}
